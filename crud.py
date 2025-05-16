ced41c09779763e174b07446b317e31f84007552
SulemanKhan01 <sulemanmusaddiq2105@gmail.com>
2025-05-17 04:04:22 +0500

    CRUD Operation code

---
+++ crud.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+from fastapi import HTTPException
+
+def get_user(db: Session, user_id: int):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+def get_users(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, user_id: int, user_data: schemas.UserUpdate):
+    user = get_user(db, user_id)
+    user.name = user_data.name
+    user.email = user_data.email
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    db.delete(user)
+    db.commit()
+    return user
